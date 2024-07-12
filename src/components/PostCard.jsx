c7933c8f238b9bf02cf3c9c96691fce3619f147a
Shashikant <142254614+Shashikant3006@users.noreply.github.com>
2024-07-12 15:01:04 +0530

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -6,39 +6,46 @@ import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import Actions from "./Actions"
-import moment from "moment"
-import {AuthContext} from '../App';
-
+import Actions from "./Actions";
+import moment from "moment";
+import { AuthContext } from '../App';
 
 export default function PostCard(props) {
-
- const {user,_id,title,content,image,createdOn} = props.post;
- const {auth} = React.useContext(AuthContext)
+  const { user, _id, title, content, image, createdOn } = props.post;
+  const { auth } = React.useContext(AuthContext);
 
   return (
-    <Card sx={{width: "100%",  borderRadius:"10px"}} id={_id} elevation={15}>
+    <Card sx={{ width: "100%", borderRadius: "10px" }} id={_id} elevation={15}>
       <CardHeader
-        avatar={ 
+        avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            {user.name.slice(0,1)}
+            {user.name.slice(0, 1)}
           </Avatar>
-         }
-        action={
-          auth._id=== user._id && <Actions id={_id}/>
         }
-        title={title}
+        action={
+          auth._id === user._id && <Actions id={_id} />
+        }
+        title={
+          <div>
+            <Typography variant="body1" color="text.primary">
+              {user.name}
+            </Typography>
+            <Typography variant="h6" color="text.primary">
+              {title}
+            </Typography>
+          </div>
+        }
         subheader={moment(createdOn).fromNow()}
       />
       <CardMedia
         component="img"
         height="100%"
         image={image}
-        alt={user}
+        alt={user.name}
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-         {content}
+          {content}
         </Typography>
       </CardContent>
     </Card>
